6ca5c585e5419ea2f984768bb64331303cb6b80c
hugosenari <hugosenari@gmail.com>
2022-03-12 01:16:48 -0300

    chore: re index cmixer_sdl2

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Lightweight audio mixer for SDL2'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."cmixer_sdl2-master".url = "path:./master";
+  inputs."cmixer_sdl2-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."cmixer_sdl2-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
